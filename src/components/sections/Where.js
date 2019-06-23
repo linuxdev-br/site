895f47b71b914e999ccea26214ec73a9416d96cc
Gustavo Padovan <gustavo.padovan@collabora.com>
2019-06-23 19:21:02 -0300

    Where: make it a real gatsby Section
    drop unneeded stuff in the process

--- src/components/sections/Where.js
+++ src/components/sections/Where.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { StaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
-import { Container } from '@components/global';
+import { Section, Container } from '@components/global';
 import ExternalLink from '@common/ExternalLink';
 
 const Where = () => (
@@ -23,39 +22,28 @@ const Where = () => (
       }
     `}
     render={data => (
-      <HeaderWrapper>
-        <Container>
-        <h1>Where</h1>
-        <br />
-          <Grid>
-          <Art>
-                <p>August 2 at CCSL/University of São Paulo</p>
+        <Section id="where" accent="secondary">
+            <Container>
+                <h1>Where</h1>
                 <br />
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.269529770778!2d-46.73362434899728!3d-23.558761384608957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce561437311a2b%3A0x281256ef0da42272!2sCCSL+-+Centro+de+Compet%C3%AAncia+em+Software+Livre+da+Universidade+de+S%C3%A3o+Paulo!5e0!3m2!1sen!2sbr!4v1561326718902!5m2!1sen!2sbr" width="400" height="300" allowfullscreen></iframe>
-
-            </Art>
-            <Art>
-                <p>August 3-4 at Radisson Paulista Plaza</p>
-                <br />
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.9110459369185!2d-46.648547348997134!3d-23.571638084602665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5996048af781%3A0x9856d09e924a4d99!2sRadisson+Paulista+Plaza!5e0!3m2!1sen!2sbr!4v1561326556291!5m2!1sen!2sbr" width="400" height="300" allowfullscreen></iframe>
-            </Art>
-            
-          </Grid>
-        </Container>
-      </HeaderWrapper>
+                 <Grid>
+                    <Art>
+                        <p>August 2 at CCSL/University of São Paulo</p>
+                        <br />
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.269529770778!2d-46.73362434899728!3d-23.558761384608957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce561437311a2b%3A0x281256ef0da42272!2sCCSL+-+Centro+de+Compet%C3%AAncia+em+Software+Livre+da+Universidade+de+S%C3%A3o+Paulo!5e0!3m2!1sen!2sbr!4v1561326718902!5m2!1sen!2sbr" width="400" height="300" allowfullscreen></iframe>
+                    </Art>
+                    <Art>
+                        <p>August 3-4 at Radisson Paulista Plaza</p>
+                        <br />
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.9110459369185!2d-46.648547348997134!3d-23.571638084602665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5996048af781%3A0x9856d09e924a4d99!2sRadisson+Paulista+Plaza!5e0!3m2!1sen!2sbr!4v1561326556291!5m2!1sen!2sbr" width="400" height="300" allowfullscreen></iframe>
+                    </Art>         
+                </Grid>
+            </Container>
+        </Section>
     )}
   />
 );
 
-const HeaderWrapper = styled.header`
-  background-color: ${props => props.theme.color.primary};
-  padding-top: 96px;
-  padding-bottom: 96px;
-
-  @media (max-width: ${props => props.theme.screen.md}) {
-    padding-top: 128px;
-  }
-`;
 
 const Art = styled.figure`
   width: 100%;
@@ -82,23 +70,6 @@ const Grid = styled.div`
     > ${Art} {
       order: 2;
     }
-  }
-`;
-
-const Text = styled.div`
-  justify-self: center;
-
-  @media (max-width: ${props => props.theme.screen.md}) {
-    justify-self: start;
-  }
-`;
-
-const StyledExternalLink = styled(ExternalLink)`
-  color: inherit;
-  text-decoration: none;
-
-  &:hover {
-    color: ${props => props.theme.color.black.regular};
   }
 `;
 
