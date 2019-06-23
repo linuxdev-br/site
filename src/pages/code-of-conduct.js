9689e26e9479a8ab237a0e1d4559459a758314a3
Gustavo Padovan <gustavo.padovan@collabora.com>
2019-06-23 14:24:08 -0300

    add code of conduct page

---
+++ src/pages/code-of-conduct.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Container } from '@components/global';
+
+import Layout from '@common/Layout';
+import Navbar from '@common/Navbar';
+
+import Header from '@sections/Header';
+import Footer from '@sections/Footer';
+
+const CodeOfConduct = () => (
+  <Layout>
+    <Navbar />
+    <Header />
+    <Container>
+        <Content>
+
+<p>Linux Developer Conference Brazil(linuxdev-br) is a community conference intended for networking and collaboration in the Linux community. We value the participation of each member of the community and want all attendees to have an enjoyable and fulfilling experience. Accordingly, all attendees are expected to show respect and courtesy to other attendees throughout the conference and at all conference events, whether officially sponsored by the conference or not.</p>
+<br /><p>To make clear what is expected, all delegates/attendees, speakers, exhibitors, organizers and volunteers are required to conform to the following Code of Conduct. Organizers will enforce this code throughout the event.</p>
+<br />
+<h1>Code of Conduct</h1>
+<br />
+<br /><p>Harassment includes offensive verbal comments related to gender, sexual orientation, disability, physical appearance, body size, race, religion, sexual images in public spaces, deliberate intimidation, stalking, persistent following, harassing photography or recording, sustained disruption of talks or other events, inappropriate physical contact, and unwelcome sexual attention.</p>
+<br /><p>Participants asked to stop any harassing behavior are expected to comply immediately. Further unacceptable behavior will result in escalating sanctions.</p>
+<br /><p>Exhibitors in the expo hall, sponsor or vendor booths, or similar activities are also subject to the anti-harassment policy. In particular, exhibitors should not use sexualized images, activities, or other material. Booth staff (including volunteers) should not use sexualized clothing/uniforms/costumes, or otherwise create an unprofessional environment.</p>
+<br /><p>Be careful in the words that you choose. Remember that sexist, racist, and other exclusionary jokes can be offensive to those around you. If you think your conversation is making another community member uncomfortable, try to make amends and move forward.</p>
+<br /><p>If a participant engages in behavior that violates this code of conduct, the conference organizers may take any action they deem appropriate, including warning the offender, expulsion from the conference with no refund, or legal action.</p>
+<br /><h3>Contact Information</h3>
+<br /><p>If you are being harassed, notice that someone else is being harassed, or have any other concerns, please contact a member of conference staff. Conference staff can be found below or you can also contact hotel staff and ask to be put in touch with the conference organizers.</p>
+<br /><p>If the matter is especially urgent, please call/contact any of these individuals:</p>
+<br /><p>Gustavo Padovan at +1 425 394 6417 (Whatsapp and Telegram)</p>
+<p>Helen Koike at +55 (19) 99917-0956</p>
+<br /><p>Conference staff will be happy to help participants contact hotel/venue security or local law enforcement, provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration of the conference. We value your attendance.</p>
+<br /><h3>License</h3>
+<br /><p>This Code of Conduct was derived from the&nbsp;<a href="https://blog.chef.io/chefconfcodeofconduct/">ChefConf Code of Conduct</a>&nbsp;which was forked from the example policy from the <a href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Policy">Geek Feminism wiki, created by the Ada Initiative and other volunteers</a>. which is under a Creative Commons Zero license. It is also based on the <a href="https://github.com/python/pycon-code-of-conduct">PyCon Code of Conduct</a>.</p>
+<br /><h2>Attendee Procedure for Incident Handling</h2>
+<br /><p>This procedure has been adopted from the Ada Initiative’s guide titled “<a href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Responding_to_reports">Conference anti-harassment/Responding to Reports</a>”.</p>
+<ol>
+<li>Keep in mind that all conference staff will be wearing a conference badge with the word “STAFF” on it (or otherwise clearly marked as staff). The staff will also be prepared to handle the incident. All of our staff are informed of the Code of Conduct policy and guide for handling harassment at the conference. <i>There will be a mandatory staff meeting onsite at the conference when this will be reiterated.</i></li>
+<li>Report the harassment incident (preferably in writing) to a conference staff member. All reports are confidential. Please do not disclose public information about the incident until the staff have had sufficient time in which to address the situation. This is as much for your safety and protection as it is the other attendees.</li>
+</ol>
+<p>When reporting the event to staff, try to gather as much information as available but do not interview people about the incident. Staff will assist you in writing the report/collecting information.</p>
+<p>The important information consists of:</p>
+<ul>
+<li>Identifying information (name/badge number) of the participant doing the harassing</li>
+<li>The behavior that was in violation</li>
+<li>The approximate time of the behavior</li>
+<li>The circumstances surrounding the incident</li>
+<li>Other people involved in the incident</li>
+</ul>
+<p>The staff is well informed on how to deal with the incident and how to further proceed with the situation.</p>
+<ol>
+<li>If everyone is presently physically safe, involve law enforcement or security only at a victim’s request. If you do feel your safety in jeopardy please do not hesitate to contact local law enforcement by dialing 911. If you do not have a cell phone, you can use any hotel phone or simply ask a staff member.</li>
+</ol>
+<p><b>Note</b>: Incidents that violate the Code of Conduct are extremely damaging to the community, and they will not be tolerated. The silver lining is that, in many cases, these incidents present a chance for the offenders, and the community at large, to grow, learn, and become better. linuxdev-br staff requests that they be your first resource when reporting a linuxdev-br-related incident, so that they may enforce the Code of Conduct and take quick action toward a resolution.</p>
+<p>If at all possible, all reports should be made directly to <a href="mailto:gustavo@padovan.org">Gustavo Padovan</a>.</p>
+<p><a name="staff_procedure"></a></p>
+<br /><h2>Staff Procedure for Incident Handling</h2>
+<br /><p>This procedure has been adopted from the Ada Initiative’s guide titled “<a href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Responding_to_reports">Conference anti-harassment/Responding to Reports</a>”.</p>
+<br /><p>Be sure to have a good understanding of our Code of Conduct policy.</p>
+<br /><p>Also have a good understanding of what is expected from an attendee that wants to report a harassment incident. These guidelines can be found in the Attendee Procedure for incident handling.</p>
+<br /><p>Try to get as much detail of the incident in written form from the reporter. If you cannot, transcribe it yourself as it was told to you. The important information to gather includes the following:</p>
+<ul>
+<li>Identifying information (name/badge number) of the participant doing the harassing</li>
+<li>The behavior that was in violation</li>
+<li>The approximate time of the behavior</li>
+<li>The approximate time the report was made</li>
+<li>The circumstances surrounding the incident</li>
+<li>Other people involved in the incident</li>
+</ul>
+<p>Prepare an initial response to the incident. This initial response is very important and will set the tone for linuxdev-br. Depending on the severity/details of the incident, please follow these guidelines:</p>
+<ul>
+<li>If there is any general threat to attendees or the safety of anyone including conference staff is in doubt, summon security or police</li>
+<li>Offer the victim a private place to sit</li>
+<li>Ask “is there a friend or trusted person who you would like to be with you?” (if so, arrange for someone to fetch this person)</li>
+<li>Ask them “how can I help?”</li>
+<li>Provide them with your list of emergency contacts if they need help later</li>
+<li>If everyone is presently physically safe, involve law enforcement or security only at a victim’s request</li>
+</ul>
+<p>There are also some guidelines as to what not to do as an initial response:</p>
+<ul>
+<li>Do not overtly invite them to withdraw the complaint or mention that withdrawal is OK. This suggests that you want them to do so, and is therefore coercive. “If you’re OK with it [pursuing the complaint]” suggests that you are by default pursuing it and is not coercive.</li>
+<li>Do not ask for their advice on how to deal with the complaint. This is a staff responsibility.</li>
+<li>Do not offer them input into penalties. This is the staff’s responsibility.</li>
+</ul>
+<p>Once something is reported to a staff member, immediately meet with the conference chair and/or event manager. The main objectives of this meeting is to find out the following:</p>
+<ul>
+<li>What happened?</li>
+<li>Are we doing anything about it?</li>
+<li>Who is doing those things?</li>
+<li>When are they doing them?</li>
+</ul>
+<br /><p>After the staff meeting and discussion, have a staff member (preferably the conference chair or event manager if available) communicate with the alleged harasser. Make sure to inform them of what has been reported about them.</p>
+<br /><p>Allow the alleged harasser to give their side of the story to the staff. After this point, if the report stands, let the alleged harasser know what actions will be taken against them.</p>
+<br /><p>Some things for the staff to consider when dealing with Code of Conduct offenders:</p>
+<ul>
+<li>Warning the harasser to cease their behavior and that any further reports will result in sanctions</li>
+<li>Requiring that the harasser avoid any interaction with, and physical proximity to, their victim for the remainder of the event</li>
+<li>Ending a talk that violates the policy early</li>
+<li>Not publishing the video or slides of a talk that violated the policy</li>
+<li>Not allowing a speaker who violated the policy to give (further) talks at the event now or in the future</li>
+<li>Immediately ending any event volunteer responsibilities and privileges the harasser holds</li>
+<li>Requiring that the harasser not volunteer for future events your organization runs (either indefinitely or for a certain time period)</li>
+<li>Requiring that the harasser immediately leave the event and not return</li>
+<li>Banning the harasser from future events (either indefinitely or for a certain time period)</li>
+<li>Publishing an account of the harassment and calling for the resignation of the harasser from their responsibilities (usually pursued by people without formal authority: may be called for if the harasser is the event leader, or refuses to stand aside from the conflict of interest, or similar, typically event staff have sufficient governing rights over their space that this isn’t as useful)</li>
+</ul>
+<br /><p>Give accused attendees a place to appeal to if there is one, but in the meantime the report stands. Keep in mind that it is not a good idea to encourage an apology from the harasser.</p>
+<br /><p>It is very important how we deal with the incident publicly. Our policy is to make sure that everyone aware of the initial incident is also made aware that it is not according to policy and that official action has been taken – while still respecting the privacy of individual attendees. When speaking to individuals (those who are aware of the incident, but were not involved with the incident) about the incident it is a good idea to keep the details out.</p>
+<br /><p>Depending on the incident, the conference chair, or designate, may decide to make one or more public announcements. If necessary, this will be done with a short announcement either during the plenary and/or through other channels. No one other than the conference chair or someone delegated authority from the conference chair should make any announcements. No personal information about either party will be disclosed as part of this process.</p>
+<br /><p>If some attendees were angered by the incident, it is best to apologize to them that the incident occurred to begin with. If there are residual hard feelings, suggest to them to write an email to the conference chair or to the event coordinator. It will be dealt with accordingly.</p>
+<br /><p>If at all possible, all reports should be made directly to <a href="mailto:gustavo@padovan.org">Gustavo Padovan</a>.</p>
+<br />
+
+        </Content>
+    </Container>
+    <Footer />
+  </Layout>
+);
+
+const Content = styled.div`
+  justify-content: space-between;
+  margin-top: 72px;
+  font-size: 15pt;
+
+  p {
+      font-size: 15pt;
+  }
+`;
+
+export default CodeOfConduct;
