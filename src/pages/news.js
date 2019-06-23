3e6f76c492afe53e45d552e7d925242343b5e360
Gustavo Padovan <gustavo.padovan@collabora.com>
2019-06-23 13:03:36 -0300

    template for the News page

---
+++ src/pages/news.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Layout from '@common/Layout';
+import Navbar from '@common/Navbar';
+
+import Header from '@sections/Header';
+import Footer from '@sections/Footer';
+
+const News = () => (
+  <Layout>
+    <Navbar />
+    <Header />
+    <Footer />
+  </Layout>
+);
+
+export default News;
