a3fd71e2ccd2a94b38dc6d32a90de62f0382b2e6
Gustavo Padovan <gustavo.padovan@collabora.com>
2019-06-23 20:21:13 -0300

    Faq: open links from Faq in the same window

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -3,15 +3,14 @@ import React from 'react';
 import { Section, Container } from '@components/global';
 
 import FaqItem from '@common/FaqItem';
-import ExternalLink from '@common/ExternalLink';
 
 const FAQS = [
   {
     title: 'Does linuxdev-br have a Code of Conduct? Where can I find it?',
     content: () => (
       <>
-        Yes, we do have a <ExternalLink href="./code-of-conduct"> Code of 
-        Conduct</ExternalLink> and all participants are required to conform to the Code of Conduct. Any kind of harrasment
+        Yes, we do have a <a href="./code-of-conduct"> Code of 
+        Conduct</a> and all participants are required to conform to the Code of Conduct. Any kind of harrasment
         or unacceptable behavior is unwelcome at this event.
       </>
     ),
@@ -20,7 +19,7 @@ const FAQS = [
     title: 'What is the Mission of the linuxdev-br conference?',
     content: () => (
       <>
-        Our <ExternalLink href="./mission">Mission</ExternalLink> is to become an international
+        Our <a href="./mission">Mission</a> is to become an international
         meeting point in Brazil for the community and industry developing Free and Open Source
         Software (FOSS) for the Core of Linux systems.
       </>
@@ -30,7 +29,7 @@ const FAQS = [
     title: 'Who is the Organizing Committee of the linuxdev-br conference?',
     content: () => (
       <>
-        See the <ExternalLink href="./committee">members</ExternalLink> of our Organizing Committee.
+        See the <a href="./committee">members</a> of our Organizing Committee.
       </>
     ),
   },
